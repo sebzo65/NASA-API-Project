a95a2f9444a536271d5ee0787b491e046136118d
Sebastian <sebzo65@gmail.com>
2021-12-09 16:45:28 +1100

    Moved the API logic for the home page components from the individual presentational components into the Home Container where all logic must reside, and kept all the styling within the individual components. Need to figure out how to filter the data I am receiving from the API by the media type

--- src/Components/AstroHeader/AstroHeader.jsx
+++ src/Components/AstroHeader/AstroHeader.jsx
@@ -1,21 +1,6 @@
-import { useState, useEffect } from "react";
 import styles from "./AstroHeader.module.scss";
 
-const AstroHeader = () => {
-  const [picture, getPicture] = useState(1);
-
-  useEffect(() => {
-    const getPicOfDay = async () => {
-      const response = await fetch(
-        `https://api.nasa.gov/planetary/apod?api_key=mgCzn4vdY8LralpHVwIKBPydQIDt6LXodanpsy10`
-      );
-      const data = await response.json();
-      getPicture(data);
-    };
-    getPicOfDay();
-  }, []);
-  console.log(picture);
-
+const AstroHeader = ({ picture, images }) => {
   return (
     <div>
       <h1 className={styles.title}>{picture.title}</h1>
--- src/Components/NASAImageGallery/NASAImageGallery.jsx
+++ src/Components/NASAImageGallery/NASAImageGallery.jsx
@@ -1,18 +1,6 @@
-import { useState, useEffect } from "react";
 import styles from "./NASAImageGallery.module.scss";
 
 const NASAImageGallery = () => {
-  const [images, getImages] = useState(2);
-
-  useEffect(() => {
-    const renderImages = async () => {
-      const response = await fetch(`https://images-api.nasa.gov/search?q=poor`);
-      const data = await response.json();
-      getImages(data.collection.items);
-    };
-    renderImages();
-  }, []);
-  console.log(images);
   return <div></div>;
 };
 

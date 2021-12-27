fa59bc8741b2ca676460d56cba3dab6477a8e928
Sebastian <sebzo65@gmail.com>
2021-12-27 16:49:31 +1100

    Continued to work on Earth API to allow user to input latitude, longitude and date to render a Landsat image of the given location at the gi ven time. Worked on all the functionality in the SearchBar, as well as in the Container to render search from API. Next step is to continue working on this in order to allow search query to take into account all of the inputs when rendering a search

--- src/Components/LandsatSearch/LandsatSearch.jsx
+++ src/Components/LandsatSearch/LandsatSearch.jsx
@@ -1,38 +1,75 @@
 import { useState } from "react";
 import styles from "./LandsatSearch.module.scss";
+import { Button } from "react-bootstrap";
 
-const LandsatSearch = () => {
+const LandsatSearch = ({ onSubmit }) => {
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
+  const [date, setDate] = useState("");
 
-  //   const handleInput = (e) => {
-  //     setLatitude(e.target.value);
-  //     setLongitude(e.target.value);
-  //   };
+  const handleLatInput = (e) => {
+    setLatitude(e.target.value);
+  };
 
-  //   const handleButtonClick = () => {
-  //     onSubmit(latitude, longitude);
-  //   };
+  const handleLongInput = (e) => {
+    setLongitude(e.target.value);
+  };
 
-  //   const handleKeyPress = (e) => {
-  //     if (e.key === "Enter") {
-  //       e.preventDefault();
-  //       handleButtonClick();
-  //     }
-  //   };
+  const handleDateInput = (e) => {
+    setDate(e.target.value);
+  };
+
+  const handleButtonClick = () => {
+    onSubmit(latitude, longitude);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleButtonClick();
+    }
+  };
 
   return (
     <div>
       <div className={styles.inputBox}>
-        <label htmlFor="input"></label>
+        <label htmlFor="latInput"></label>
         <input
           type="text"
-          //   value={inputValue}
-          //   onChange={handleInput}
-          //   onKeyPress={handleKeyPress}
-          className={styles.input}
-          name="input"
+          value={latitude}
+          onChange={handleLatInput}
+          onKeyPress={handleKeyPress}
+          className={styles.latInput}
+          name="latInput"
         ></input>
+        <Button
+          onClick={handleButtonClick}
+          className={styles.button}
+          variant="secondary"
+          size="lg"
+        >
+          SEARCH
+        </Button>
+        <label htmlFor="longInput"></label>
+        <input
+          type="text"
+          value={longitude}
+          onChange={handleLongInput}
+          onKeyPress={handleKeyPress}
+          className={styles.longInput}
+          name="longInput"
+        ></input>
+        <div>
+          <label htmlFor="dataInput"></label>
+          <input
+            type="text"
+            value={date}
+            onChange={handleDateInput}
+            onKeyPress={handleKeyPress}
+            className={styles.dateInput}
+            name="dateInput"
+          ></input>
+        </div>
       </div>
     </div>
   );
--- src/Containers/LandsatImages/LandsatImages.jsx
+++ src/Containers/LandsatImages/LandsatImages.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import LandsatSearch from "../../Components/LandsatSearch";
 
 const LandsatImages = () => {
+  //Longitude SearchBar functionality for Earth API
+  const [longitude, setLongitude] = useState(100.75);
+  const [latitude, setLatitude] = useState(1.5);
+  const [date, setDate] = useState("2014-02-01");
+  //
+  const handleSubmit = (newSearchValue) => {
+    setLongitude(newSearchValue);
+    setLatitude(newSearchValue);
+    setDate(newSearchValue);
+  };
+
+  //Earth API (Landsat Images from coordinates)
+  const [landsat, setLandsat] = useState(null);
+
+  useEffect(() => {
+    const renderLandsat = async () => {
+      const response = await fetch(
+        `https://api.nasa.gov/planetary/earth/imagery?lon=${longitude}&lat=${latitude}&${date}&api_key=dNmdVqWGeHJ2klLNWXyfZfSqUX6OB4IJXqmw6dlZ`
+      );
+      const landsatData = await response.json();
+      setLandsat(landsatData);
+    };
+    renderLandsat();
+  }, [longitude, latitude, date]);
+  console.log(landsat);
   return (
     <div>
-      <LandsatSearch />
+      <LandsatSearch onSubmit={handleSubmit} />
     </div>
   );
 };

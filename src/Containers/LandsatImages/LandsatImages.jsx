7fb148ac52ccb991fbef722d53f69a34b03aa0da
Sebastian <sebzo65@gmail.com>
2022-01-06 16:35:19 +1100

    Started working on the search functionality for rendering Landsat images to the page based on user input. Finished the input search bar for longitude. Next step is to create the functionality for the latitude searchbar & to ensure this works in tandem with the longitude input value

--- src/Containers/LandsatImages/LandsatImages.jsx
+++ src/Containers/LandsatImages/LandsatImages.jsx
@@ -1,20 +1,14 @@
 import { useState, useEffect } from "react";
-// import LandsatSearch from "../../Components/LandsatSearch";
+import LandsatSearch from "../../Components/LandsatSearch";
 import styles from "./LandsatImages.module.scss";
 
 const key = "GJ18rhBDoC1WeHfexEhTSsZh18xsgmlr3w25Rr3i";
-
 const LandsatImages = () => {
-  //Longitude SearchBar functionality for Earth API
-  // const [longitude, setLongitude] = useState(100.75);
+  //Longitude, Latitude & Date SearchBar functionality for Earth API
+  const [longitude, setLongitude] = useState(-110.8358417);
   // const [latitude, setLatitude] = useState(1.5);
   // const [date, setDate] = useState("2014-02-01");
   //
-  // const handleSubmit = (newSearchValue) => {
-  //   setLongitude(newSearchValue);
-  //   setLatitude(newSearchValue);
-  //   setDate(newSearchValue);
-  // };
 
   //Earth API (Landsat Images from coordinates)
   const [landsat, setLandsat] = useState(null);
@@ -22,24 +16,31 @@ const LandsatImages = () => {
   useEffect(() => {
     const renderLandsat = async () => {
       const response = await fetch(
-        `https://api.nasa.gov/planetary/earth/assets?lon=-110.8358417
-        &lat=32.1499889
-        &date=2018-11-01&&dim=0.155&api_key=${key}`
-
-        // `https://api.nasa.gov/planetary/earth/imagery?lon=${longitude}&lat=${latitude}&${date}&api_key=dNmdVqWGeHJ2klLNWXyfZfSqUX6OB4IJXqmw6dlZ`
+        `https://api.nasa.gov/planetary/earth/assets?lon=${longitude}&lat=32.1499889&date=2020-11-01&&dim=0.155&api_key=${key}`
       );
+      console.log("response: ", response);
       const landsatData = await response.json();
+      console.log("landsatData: ", landsatData);
       setLandsat(landsatData);
     };
     renderLandsat();
-  }, []);
+  }, [longitude]);
   // }, [longitude, latitude, date]);
   console.log("Landsat-Imagery state: ", landsat);
+
+  console.log(longitude);
+  const handleSubmit = (newSearchValue) => {
+    setLongitude(newSearchValue);
+    // setLatitude(newSearchValue);
+    // setDate(newSearchValue);
+  };
+
   return (
     <div>
       <h1 className={styles.title}>
         Find satellite images of your favourite place!
       </h1>
+      <LandsatSearch onSubmit={handleSubmit} />
       {landsat && (
         <img src={landsat.url} alt="test" className={styles.landsatPic} />
       )}

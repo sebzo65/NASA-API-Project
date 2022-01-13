862e25bdccb087132cd423b27615d8bbd62efb05
Sebastian <sebzo65@gmail.com>
2022-01-13 16:35:44 +1100

    Fixed bug and got both the latitude and longitude inputs working for the landsat imagery page. Next step is to create the keypress function that enables search upon tapping the enter button, as well as providing useful information for users such as valid latitude and longitude ranges, and interesting locations that they can search

--- src/Components/LandsatSearch/LandsatSearch.jsx
+++ src/Components/LandsatSearch/LandsatSearch.jsx
@@ -1,75 +1,46 @@
 import { useState } from "react";
 import styles from "./LandsatSearch.module.scss";
-import { Button } from "react-bootstrap";
 
 const LandsatSearch = ({ onSubmit }) => {
-  // const [latitudeInput, setLatitudeInput] = useState("");
-  const [longitudeInput, setLongitudeInput] = useState("");
-  // const [dateInput, setDateInput] = useState("");
-
-  // const handleLatInput = (e) => {
-  //   setLatitudeInput(e.target.value);
-  // };
-
-  const handleLongChange = (e) => {
-    setLongitudeInput(e.target.value);
+  // state for long value
+  const [longValue, setLongValue] = useState("");
+  //handler to keep track of input value
+  const handleLongChange = (event) => {
+    setLongValue(event.target.value);
   };
-
-  // const handleDateInput = (e) => {
-  //   setDateInput(e.target.value);
-  // };
-
+  // update our search state after clicking button
   const handleButtonClick = () => {
-    onSubmit(longitudeInput);
+    onSubmit(longValue);
+    onSubmit(latValue);
   };
 
-  const handleKeyPress = (e) => {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      handleButtonClick();
-    }
+  console.log("longValue", longValue);
+
+  // state for lat value
+  const [latValue, setLatValue] = useState("");
+  // handler to track lat input value
+  const handleLatChange = (event) => {
+    setLatValue(event.target.value);
   };
+  console.log("latValue", latValue);
 
   return (
-    <div>
-      {/* <div className={styles.inputBox}>
-        <label htmlFor="latInput"></label>
-        <input
-          type="text"
-          value={latitude}
-          onChange={handleLatInput}
-          onKeyPress={handleKeyPress}
-          className={styles.latInput}
-          name="latInput"
-        ></input>*/}
-      <Button
-        onClick={handleButtonClick}
-        className={styles.button}
-        variant="secondary"
-        size="lg"
-      >
-        SEARCH
-      </Button>
-      <label htmlFor="longInput"></label>
-      <input
-        type="text"
-        onChange={handleLongChange}
-        onKeyPress={handleKeyPress}
-        className={styles.longInput}
-        name="longInput"
-      ></input>
-      {/*<div>
-          <label htmlFor="dataInput"></label>
-          <input
-            type="text"
-            value={date}
-            onChange={handleDateInput}
-            onKeyPress={handleKeyPress}
-            className={styles.dateInput}
-            name="dateInput"
-          ></input>
-        </div>
-      </div> */}
+    <div className={styles.searchBar}>
+      <div className={styles.searchBar_long}>
+        <label>Longitude</label>
+        <input type="text" onChange={handleLongChange} />
+      </div>
+      <div>
+        <label>Latitude</label>
+        <input type="text" onChange={handleLatChange} />
+      </div>
+      {/* <div>
+                <p>Date</p>
+                <input type="text" />
+            </div> */}
+      <div>
+        <button onClick={handleButtonClick}>Search!</button>
+      </div>
     </div>
   );
 };

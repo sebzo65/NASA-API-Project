d6fcad09121807a9f32b5d196f4a4db3a22422c6
Sebastian <sebzo65@gmail.com>
2022-02-08 16:09:09 +1100

    Worked with the Mars rover API and managed to console log the output from the API. Also mapped through the images inside the object array in order to filter and select images. The code is functioning soundly, however there is a network connection issue that I am yet to resolve. In other words, the data appears in the console, but does not output to the page

--- src/Components/RoverPics/RoverPics.jsx
+++ src/Components/RoverPics/RoverPics.jsx
@@ -1,46 +1,8 @@
-import { Carousel } from "react-bootstrap";
+import { styles } from "./RoverPics.module.scss";
 
 const RoverPics = ({ roverPics }) => {
-  return (
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://scx2.b-cdn.net/gfx/news/2018/meteoritesre.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://www.thestatesman.com/wp-content/uploads/2017/08/1487141680-marsgetty517.jpg"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2016/02/arda_valles_perspective/15820153-1-eng-GB/Arda_Valles_perspective_pillars.jpg"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  );
+  console.log("roverPics prop: ", roverPics);
+  return <div>{<img src={roverPics.img_src} alt="" />}</div>;
 };
 
 export default RoverPics;

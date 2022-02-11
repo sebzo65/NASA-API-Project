1d26f34a56e3281f6ac7837090fc4983132f7bab
Sebastian <sebzo65@gmail.com>
2022-02-11 16:43:54 +1100

    Cleaned up the layout of the data rendered from the Mars Rover API, added some Bootstrap radio buttons and attempted to create a handleClick function which changes the images displayed based on the Rover camera selected. Also, initially I thought there were doubles of some of the images in the database and so attempted to clean up the data with a pure function, but they actually all have separate ids. Next step is to finish the handleClick functionality

--- src/Containers/MarsRover/MarsRover.jsx
+++ src/Containers/MarsRover/MarsRover.jsx
@@ -1,29 +1,68 @@
 import { useState, useEffect } from "react";
 import RoverPics from "../../Components/RoverPics";
+import styles from "./MarsRover.module.scss";
+import { Button, ButtonGroup } from "react-bootstrap";
 
 const MarsRover = () => {
   //Mars Rover API
   const [roverPics, setRoverPics] = useState(null);
+
+  const [roverCam, setRoverCam] = useState(null);
 
   // const KEY = "GJ18rhBDoC1WeHfexEhTSsZh18xsgmlr3w25Rr3i";
 
   useEffect(() => {
     const getRoverPics = async () => {
       const response = await fetch(
-        `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=NAVCAM&api_key=DEMO_KEY`
+        `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&&camera=${roverCam}&api_key=DEMO_KEY`
       );
       const data = await response.json();
       setRoverPics(data);
     };
     getRoverPics();
-  }, []);
-  console.log(roverPics);
+  }, [roverCam]);
+
+  const handleClick = (e) => {
+    setRoverCam(e.target.value);
+  };
+
+  // const cleanedRoverPics = (data) => {
+  //   let newArr = [];
+  //   if (data !== null && data.photos.length > 0) {
+  //     for (let i = 0; i < data.photos.length; i++) {
+  //       if (data.photos[i].id !== data.photos[i + 1].id) {
+  //         newArr.push(data.photos[i]);
+  //       }
+  //     }
+  //   }
+  //   return newArr;
+  // };
+
+  // if (roverPics !== null) {
+  //   console.log(cleanedRoverPics(roverPics));
+  // }
+
+  // let newRoverPics = cleanedRoverPics(roverPics.photos);
+
   return (
     <div>
-      {roverPics &&
-        roverPics.photos
-          .filter((pics, index) => index < 3)
-          .map((pic, index) => <RoverPics roverPics={pic} key={index} />)}
+      <ButtonGroup aria-label="Basic example">
+        <Button variant="secondary" onClick={handleClick}>
+          FHAZ
+        </Button>
+        <Button variant="secondary" onClick={handleClick}>
+          NAVCAM
+        </Button>
+        <Button variant="secondary" onClick={handleClick}>
+          MAST
+        </Button>
+      </ButtonGroup>
+      <div className={styles.roverGallery}>
+        {roverPics &&
+          roverPics.photos
+            .filter((pics, index) => index < 4)
+            .map((pic, index) => <RoverPics roverPics={pic} key={index} />)}
+      </div>
     </div>
   );
 };

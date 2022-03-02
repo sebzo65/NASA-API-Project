9794995690714f114767661a9ddbde67d40e5eb6
Sebastian <sebzo65@gmail.com>
2022-03-02 14:39:17 +1100

    Fixed the issue preventing data from being rendered appropriately to the tables. Now rendering all the information I need to the tables and got the limit by number of days filter to work. Next step is to implement a randomiser function so that the results are randomised

--- src/Components/NaturalEvents/NaturalEvents.jsx
+++ src/Components/NaturalEvents/NaturalEvents.jsx
@@ -6,41 +6,56 @@ const NaturalEvents = ({ natEvent }) => {
     <div>
       <Table striped bordered hover variant="dark">
         <thead>
-          <tr>
-            <th>{natEvent.title}</th>
-            <th>{natEvent.geometry[0].date}</th>
-            <th>{natEvent.geometry[1].date}</th>
-            <th>{natEvent.geometry[2].date}</th>
-          </tr>
+          {natEvent.geometry[0].date && natEvent.geometry[1].date && (
+            <tr>
+              <th>{natEvent.title}</th>
+              <th>{natEvent.geometry[0].date}</th>
+              <th>{natEvent.geometry[1].date}</th>
+            </tr>
+          )}
         </thead>
         <tbody>
-          <tr>
-            <td>Coordinates:</td>
-            <td>
-              Lat: {natEvent.geometry[0].coordinates[0]}, Long:
-              {natEvent.geometry[0].coordinates[1]}
-            </td>
-            <td>
-              Lat: {natEvent.geometry[1].coordinates[0]}, Long:
-              {natEvent.geometry[1].coordinates[1]}
-            </td>
-          </tr>
-          <tr>
-            <td>Magnitude Value:</td>
-            <td>
-              {natEvent.geometry[0].magnitudeValue}
-              {natEvent.geometry[0].magnitudeUnit}
-            </td>
-            <td>
-              {natEvent.geometry[1].magnitudeValue}
-              {natEvent.geometry[1].magnitudeUnit}
-            </td>
-          </tr>
-          <tr>
-            <td>Source</td>
-            <td>{natEvent.sources[0]}</td>
-            <td>@twitter</td>
-          </tr>
+          {natEvent.geometry[0].coordinates[0] &&
+            natEvent.geometry[0].coordinates[1] &&
+            natEvent.geometry[1].coordinates[0] &&
+            natEvent.geometry[1].coordinates[1] && (
+              <tr>
+                <td>Coordinates:</td>
+                <td>
+                  Lat: {natEvent.geometry[0].coordinates[0]}, Long:
+                  {natEvent.geometry[0].coordinates[1]}
+                </td>
+                <td>
+                  Lat: {natEvent.geometry[1].coordinates[0]}, Long:
+                  {natEvent.geometry[1].coordinates[1]}
+                </td>
+              </tr>
+            )}
+          {natEvent.geometry[0].magnitudeValue &&
+            natEvent.geometry[1].magnitudeValue &&
+            natEvent.geometry[0].magnitudeUnit &&
+            natEvent.geometry[1].magnitudeUnit && (
+              <tr>
+                <td>Magnitude Value:</td>
+                <td>
+                  {natEvent.geometry[0].magnitudeValue}
+                  {natEvent.geometry[0].magnitudeUnit}
+                </td>
+                <td>
+                  {natEvent.geometry[1].magnitudeValue}
+                  {natEvent.geometry[1].magnitudeUnit}
+                </td>
+              </tr>
+            )}
+          {natEvent.sources[0].url && (
+            <tr>
+              <td>Source</td>
+              <td>
+                <a href={natEvent.sources[0].url}>Source Link: Click here</a>
+              </td>
+              <td>@Twitter</td>
+            </tr>
+          )}
         </tbody>
       </Table>
     </div>
